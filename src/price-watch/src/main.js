380c6d2b00a91b08f3754a147a17416a28a304b8
bg0jr <bg0jr@shared-files.de>
2024-03-15 17:39:27 +0100

    all unnecessary content removed

--- src/price-watch/src/main.js
+++ src/price-watch/src/main.js
@@ -1,8 +1,19 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from './views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    }
+  ]
+})
 
 const app = createApp(App)
 
