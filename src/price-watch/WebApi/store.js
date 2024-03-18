dd521859dd6c677d01e9000c09bc9b102669ce1e
bg0jr <bg0jr@shared-files.de>
2024-03-18 09:34:41 +0100

    load coins and stocks from config

--- src/price-watch/WebApi/store.js
+++ src/price-watch/WebApi/store.js
@@ -1,25 +1,39 @@
+const fs = require('fs')
+const path = require('path')
+
+const configHome = process.env.RAYNMAKER_CONFIG
+
+let config = {
+  coins: [],
+  stocks: []
+}
+
+if (configHome) {
+  const fullPath = path.join(configHome, 'price-watch.json')
+
+  if (fs.existsSync(fullPath)) {
+    const rawData = fs.readFileSync(fullPath)
+    config = JSON.parse(rawData)
+    console.log('Config loaded from', fullPath)
+  } else {
+    console.log('No configuration found at', fullPath)
+  }
+}
 
 // id is CoinMarketCap internal ID
-exports.coins = {
-  Bitcoin: 1,
-  Etherium: 1027,
-  Cardano: 2010,
-  Polygon: 3890,
-  Litecoin: 2,
-  Harmony: 3945,
-  Stellar: 512,
-  VeChain: 3077,
-  PolkaDot: 6636,
-  ChainLink: 1975,
-  Solana: 5426,
-  ImmutableX: 10603
+const coins = {
+  Bitcoin: '1',
+  Etherium: '1027',
+  Cardano: '2010'
 }
 
-exports.stocks = {
-  BrookfieldRenewable: 'BEPC',
-  Nel: 'D7G.XFRA',
-  Siemens: 'SIE.XETRA',
-  SiemensEnergy: 'ENR.XETRA',
-  SiemensHealthineers: 'SHL.XFRA'
+config.coins.forEach((x) => (coins[x.name] = x.id))
+
+const stocks = {
+  Siemens: 'SIE.XETRA'
 }
 
+config.stocks.forEach((x) => (stocks[x.name] = x.id))
+
+exports.coins = coins
+exports.stocks = stocks
